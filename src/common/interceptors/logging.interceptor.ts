488c0185307f204eed81b655523e3b7df1fe688d
Svet-Dovbnia <svet.dovbnia@gamibear.com>
2026-04-19 19:10:53 +0200

    include HTTP status code in access logs

--- src/common/interceptors/logging.interceptor.ts
+++ src/common/interceptors/logging.interceptor.ts
@@ -5,7 +5,7 @@ import {
   Logger,
   NestInterceptor,
 } from '@nestjs/common';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
@@ -14,14 +14,17 @@ export class LoggingInterceptor implements NestInterceptor {
   private readonly logger = new Logger(LoggingInterceptor.name);
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
-    const request = context.switchToHttp().getRequest<Request>();
-    const { method, url } = request;
+    const http = context.switchToHttp();
+    const { method, url } = http.getRequest<Request>();
+    const response = http.getResponse<Response>();
     const start = Date.now();
 
     return next.handle().pipe(
       tap(() => {
         const duration = Date.now() - start;
-        this.logger.log(`${method} ${url} - ${duration}ms`);
+        this.logger.log(
+          `${method} ${url} ${response.statusCode} - ${duration}ms`,
+        );
       }),
     );
   }
